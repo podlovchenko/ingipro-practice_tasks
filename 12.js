d495ce547eac6f462162c43765b299e66d0542c8
podlovchenko <podlovchenko.vera@gmail.com>
2017-08-03 10:52:10 +0300

    Update 12.js

--- 12.js
+++ 12.js
@@ -2,7 +2,7 @@
 
 let str = 'Мама мыла раму';
 
-let new_str = (str, maxlength) => (str.length > maxlength) ? str.slice(0, maxlength - 3) + '...' : str;
+let new_str = (str, maxlength) => (str.length > maxlength) ? str.slice(0, maxlength > 3 ? maxlength - 3 : maxlength) + '...' : str;
 	
 
-console.log(new_str(str, 7));
+console.log(new_str(str, 4));
