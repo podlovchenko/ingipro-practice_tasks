399d7b4a16b83761f87c5f503e83014976454723
podlovchenko <podlovchenko.vera@gmail.com>
2017-08-03 15:07:40 +0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,3 +1,13 @@
+/* 2
+Написать функцию palindrome, которая будет возвращает true, если переданная строка является полиндромом, и false, если не является
+function palindrome(str) {
+  // your code
+}
+palindrome("eye"); // true
+palindrome("hello"); // false
+palindrome("А роза упала на лапу Азора"); // true
+*/
+
 function palindrome(str) {
   let tmp = str.replace(/\s*/g,"");
   for (let i = 0, j = tmp.length - 1; i < j; i++, j--) {
