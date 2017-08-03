daf55464c2ec64c85bd3975b1088e85f6b108dc6
podlovchenko <podlovchenko.vera@gmail.com>
2017-08-03 11:00:08 +0300

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,24 +1,23 @@
-function range(start, stop, step) {
-	if (arguments.length < 3) {
-		step = 1;
-		if (arguments.length < 2) {
-			stop = start;
-			start = 0;
-		}
+'use strict'
+
+function range(start, stop, step = 1) {
+	if (arguments.length < 2) {
+		stop = start;
+		start = 0;
 	}
 
-	if (stop < start)
+	if (stop < start) {
 		throw new Error("error!");
+    }
 
-		var arr = [];
+	var arr = [];
 
-		while (start < stop) {
-			arr.push(start);
-			start += step;
-		}
+	while (start < stop) {
+		arr.push(start);
+		start += step;
+	}
 
 	return arr;
-	}
+}
 
-
-console.log(range(0,10,3));
+console.log(range(10, 20));
