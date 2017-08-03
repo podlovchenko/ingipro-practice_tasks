84d9da1a2fa6f0850740353f882729041f06509c
podlovchenko <podlovchenko.vera@gmail.com>
2017-08-03 11:03:29 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,10 +1,10 @@
 const arr = [1, 2, 3, 4, 5];
 
 function drop(arr, i) {
-	let new_arr = arr;
+	let new_arr = arr.slice();
 	new_arr.splice(i, 1);
 	return new_arr;
 }
 
-console.log(arr);
 console.log(drop(arr, 3));
+console.log(arr);
