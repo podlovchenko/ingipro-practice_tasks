a1454fa20c3564d9300936652fcad38e0811873d
podlovchenko <podlovchenko.vera@gmail.com>
2017-08-04 11:22:49 +0300

    bbb

---
+++ 5.js
@@ -0,0 +1,25 @@
+/* 5
+Написать функцию, которая в переданной строке str меняет слово before на слово after и возвращает новую строку.
+Если слово before начиналось с заглавной буквы, вставленное вместо него слово after тоже должно начинаться с заглавной буквы
+*/
+
+function myReplace(str, before, after) {
+  let iBefore = str.indexOf(before);
+  
+  if (iBefore === -1) {
+    throw new Error("error");
+  }
+  
+  if ((/^[A-Z]*$/.test(before[0]))) {
+    after = after[0].toUpperCase() + after.slice(1);
+  } else {
+    after = after[0].toLowerCase() + after.slice(1);
+  }
+  
+  return str.slice(0, iBefore) + after + str.slice(iBefore + before.length); 
+}
+
+console.log(myReplace("Let us get back to more Coding", "Coding", "algorithms"));
+// "Let us get back to more Algorithms".
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
+// "A quick brown fox leaped over the lazy dog"
