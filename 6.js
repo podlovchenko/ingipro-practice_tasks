0f1d34dbe77c41a90f95ae7e1c31db7882575d8b
podlovchenko <podlovchenko.vera@gmail.com>
2017-08-08 15:06:44 +0300

    Update 6.js

--- 6.js
+++ 6.js
@@ -1,53 +1,21 @@
-function deepCopy(val) {
-  let obj = {};
-
-  for (let key in val) {
-    if (val[key] instanceof Array) {
-      obj[key] = this.deepCopyArray(val[key]);
-      continue;
-    }
-    
-    if (val[key] instanceof Object) {
-      obj[key] = this.deepCopy(val[key]);
-      continue;
-    }
-    
-    obj[key] = val[key];
-  }
-  
-  return obj;
-}
-
-function deepCopyArray(val) {
-  let arr = [];
-  
+function getUrl(obj, path, defaultValue) {
+  let arr = path.split(', ');
   let i = 0;
   
-  for (let key in val) {
-    arr[i++] = deepCopy(val[key]);
+  while (obj != null && i < arr.length) {
+    obj = obj[arr[i++]];
   }
-
-  return arr;
-}
-
-function getUrl(mike) {
-  if (mike.friends[0] == undefined || mike.friends[0].avatar.url == undefined)
-    return 'http://default/url/to/avatar';
     
-  return mike.friends[0].avatar.url;
+  return obj != null ? obj : defaultValue;
 }
 
-let mike = '{\
-   "friends": [{\
-       "name": "Anna",\
-       "avatar": {\
-           "url": "http://some/url/to/avatar"\
-       }\
-   }]\
-}';
+var mike = {
+    friends: [{
+        name: 'Anna',
+        avatar: {
+            url: 'http://some/url/to/avatar'
+        }
+    }]
+};
 
-mike = JSON.parse(mike);
-
-mike = deepCopy(mike);
-
-console.log(getUrl(mike));
+console.log(getUrl(mike, 'friends, 0, avatar, url', 'http://default/url/to/avatar'));
