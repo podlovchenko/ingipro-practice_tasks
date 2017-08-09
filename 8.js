97725a42c25a3d492a3d020d007c9cb46d0bcc9b
podlovchenko <podlovchenko.vera@gmail.com>
2017-08-09 18:58:44 +0300

    Update 8.js

--- 8.js
+++ 8.js
@@ -1,3 +1,22 @@
+/* 8
+Напишите функцию getImage(url), которая возвращает промис, переходящий в состояние "resolved",
+если картинка успешно загрузилась и в "rejected" если при загрузке произошла ошибка.
+Эту функцию можно использовать так:
+function getImage(url) {
+    // your code
+}
+getImage('http://bit.ly/2vntYlL').then(url => {
+    alert(`${url} successfully loaded`);
+}).catch(url => {
+    alert(`Error loading ${url}`);
+});
+Немного теории:
+Про объект картинки можно почитать тут https://developer.mozilla.org/ru/docs/Web/API/HTMLImageElement/Image
+У объекта класса Image есть методы onload и onerror. 
+onload вызывается, если картинка успешно загружена,
+onerror вызывается в случае ошики
+*/
+
 'use strict';
 
 function getImage(url) {
