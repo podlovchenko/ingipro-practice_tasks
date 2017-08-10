8c8d6db9e208df06107d18a75cf977df3e341e25
podlovchenko <podlovchenko.vera@gmail.com>
2017-08-10 21:27:25 +0300

    Update 3.3.js

--- 3.3.js
+++ 3.3.js
@@ -2,10 +2,10 @@
 
 let arr = prompt('Введите названия городов!', 'Moscow').split(',');
 
-arr.forEach(function(item) {
+Promise.all(arr.map(item => {
 fetch(`http://api.openweathermap.org/data/2.5/weather?q=${item.trim()}&appid=bd5e378503939ddaee76f12ad7a97608`).then(
   response => response.json()).then(
-  function(json) {
+  json => {
   	console.log(`${item.trim()}: температура: ${Math.round(json.main.temp - 273)}°, скорость ветра: ${json.wind.speed} м/с, влажность: ${json.main.humidity}%`);
   }).catch(error => console.error(`Error: ${error.message}`));
-});
+}));
