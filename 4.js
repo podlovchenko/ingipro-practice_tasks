3735432ae2f47956460ce9ea294cbcfc45f9a996
podlovchenko <podlovchenko.vera@gmail.com>
2017-08-16 21:57:13 +0300

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,16 +1,27 @@
+'use strict';
+
 const arr = [1,1,2,-2,5,2,4,4,-1,-2,5];
 
 function find_number(arr) {
-	for (let i = 0; i < arr.length; i++) {
-		let count = 0;
-		for (let j = 0; j < arr.length; j++) {
-			if (arr[j] == arr[i])
-				count++;
-		}
-		if (count % 2 == 1)
-			return arr[i];
-	}
-	return null;
+    let map = new Map();
+  
+    arr.forEach(item => {
+      if (map.get(item) === undefined) {
+        map.set(item, 0);
+      }
+      map.set(item, map.get(item) + 1);
+    });
+
+    let answer = null;
+  
+    map.forEach((value, key) => {
+      if (value % 2 == 1) {
+        answer = key;
+        return;
+      }
+    });
+
+    return answer;
 }
 
 console.log(find_number(arr));
